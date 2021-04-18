3b014f6e19bc9d048f6b4bf6365bec3c0d2fc501
Adam <adamblas97@gmail.com>
2021-04-18 18:01:12 +0200

    fixed command binding

---
+++ Ex.1/Logic Layer/MainLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LogicLayer
+{
+    class MainLogic : Interfaces.IMainLogic
+    {
+        public void FetchBooksCommand()
+        {
+            MessageBox.Show("Hallo Books");
+        }
+
+        public void FetchDiscountCodesCommand()
+        {
+            MessageBox.Show("Hallo Codes");
+        }
+
+        public void FetchUsersCommand()
+        {
+            MessageBox.Show("Hallo Users");
+        }
+    }
+}
---
+++ Ex.1/TPUM/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using ViewLayer.Commands;
+
+namespace ViewLayer.ViewModel
+{
+    public class MainViewModel
+    {
+        public ICommand FetchUsersCommand => new Command(FetchUsers);
+        public ICommand FetchBooksCommand => new Command(FetchBooks);
+        public ICommand FetchDiscountCodesCommand => new Command(FetchDiscountCodes);
+       
+        private void FetchUsers()
+        {
+            MessageBox.Show("1");
+        }       
+        private void FetchBooks()
+        {
+            MessageBox.Show("2");
+        }        
+        private void FetchDiscountCodes()
+        {
+            MessageBox.Show("3");
+        }
+    }
+}
