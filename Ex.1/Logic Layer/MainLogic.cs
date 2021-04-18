63df6965bd82c2e90387136b5fbd3b08e9dc5708
Adam <adamblas97@gmail.com>
2021-04-18 21:33:17 +0200

    Added crud

--- Ex.1/Logic Layer/MainLogic.cs
+++ Ex.1/Logic Layer/MainLogic.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using DataLayer;
+using DataLayer.Model;
 using DataLayer.Repositories.Books;
 using DataLayer.Repositories.DiscountCodes;
 using DataLayer.Repositories.Users;
@@ -50,6 +52,82 @@ namespace LogicLayer
         public IEnumerable<DiscountCodeDTO> GetAllDiscountCodes()
         {
             return discountCodeRepo.Items.Select(DTOMapper.DiscountCode2DTO);
+        }
+
+        public BookDTO GetBook(Guid id)
+        {
+            Book book = bookRepo.Find(book => book.Id.Equals(id));
+            if (book == null)
+                return null;
+            return DTOMapper.Book2DTO(book);
+        }
+        public UserDTO GetUser(Guid id)
+        {
+            User user = userRepo.Find(user => user.Id.Equals(id));
+            if (user == null)
+                return null;
+            return DTOMapper.User2DTO(user);
+        }
+        public DiscountCodeDTO GetDiscountCode(Guid id)
+        {
+            DiscountCode code = discountCodeRepo.Find(code => code.Id.Equals(id));
+            if (code == null)
+                return null;
+            return DTOMapper.DiscountCode2DTO(code);
+        }
+
+        public void AddBook(BookDTO dto)
+        {
+            Book book = DTOMapper.DTO2Book(dto);
+            bookRepo.Create(book);
+        }
+        public void AddUser(UserDTO dto)
+        {
+            User user = DTOMapper.DTO2User(dto);
+            userRepo.Create(user);
+        }
+        public void AddDiscountCode(DiscountCodeDTO dto)
+        {
+            DiscountCode code = DTOMapper.DTO2DiscountCode(dto);
+            discountCodeRepo.Create(code);
+        }
+
+        public void DeleteBook(Guid id)
+        {
+            BookDTO book = GetBook(id);
+            if (book == null)
+                return;
+            bookRepo.Delete(DTOMapper.DTO2Book(book));
+        }
+        public void DeleteUser(Guid id)
+        {
+            UserDTO user = GetUser(id);
+            if (user == null)
+                return;
+            userRepo.Delete(DTOMapper.DTO2User(user));
+        }
+        public void DeleteDiscountCode(Guid id)
+        {
+            DiscountCodeDTO code = GetDiscountCode(id);
+            if (code == null)
+                return;
+            discountCodeRepo.Delete(DTOMapper.DTO2DiscountCode(code));
+        }
+
+        public void UpdateBook(BookDTO updatedBookDTO)
+        {
+            Book updatedBook = DTOMapper.DTO2Book(updatedBookDTO);
+            bookRepo.Update(updatedBook);
+        }
+        public void UpdateUser(UserDTO updatedUserDTO)
+        {
+            User updatedUser = DTOMapper.DTO2User(updatedUserDTO);
+            userRepo.Update(updatedUser);
+        }
+        public void UpdateDiscountCode(DiscountCodeDTO updatedCodeDTO)
+        {
+            DiscountCode updatedCode = DTOMapper.DTO2DiscountCode(updatedCodeDTO);
+            discountCodeRepo.Update(updatedCode);
         }
     }
 }
